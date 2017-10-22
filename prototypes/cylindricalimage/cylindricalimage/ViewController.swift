46a553082ed19e0314abfe0853ef9dc590c26300
ishanivyas <ishanivyas@gmail.com>
2017-10-21 23:36:12 -0700

    Template application
    This is template application for prototype cylindrical image captrued and stitching

---
+++ prototypes/cylindricalimage/cylindricalimage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cylindricalimage
+//
+//  Created by Ishani Vyas on 10/21/17.
+//  Copyright © 2017 Ishani Vyas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
