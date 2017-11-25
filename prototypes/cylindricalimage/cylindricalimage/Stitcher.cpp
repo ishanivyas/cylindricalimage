9b45d25c4d7385e714d6c228d06164ec35b2f100
ishanivyas <ishanivyas@gmail.com>
2017-11-25 14:08:58 -0800

    Split the "warpToImages" function into two functions "findWarpSizeAndMin" and "mergeImageIntoPano".  This split enables us to begin to apply the "warp" (homography then translation) to the image separately from determining the transformation and the resulting image size.

--- prototypes/cylindricalimage/cylindricalimage/Stitcher.cpp
+++ prototypes/cylindricalimage/cylindricalimage/Stitcher.cpp
@@ -7,7 +7,7 @@ void Stitcher::filterMatchesAndExtractPoints(KeyPoints &kp1, KeyPoints &kp2, Des
     auto n = fmin(kp1.size(), kp2.size());
     for(int i = 0; i < n; i++) {
         ImgCoord src = kp1[matches[i][0].queryIdx].pt,
-                 dst = kp2[matches[i][0].trainIdx].pt;
+        dst = kp2[matches[i][0].trainIdx].pt;
         //-auto diff = sqrt(fabs(src.x - dst.x) + fabs(src.y - dst.y));
         auto diff = fabs(src.y - dst.y);
         if (diff < threshold) {
@@ -19,24 +19,26 @@ void Stitcher::filterMatchesAndExtractPoints(KeyPoints &kp1, KeyPoints &kp2, Des
 }
 
 #define len(array) ((sizeof(array) / sizeof(array[0])))
-void Stitcher::warpTwoImages(cv::InputArray img1, cv::InputArray img2, cv::Mat &H, cv::OutputArray out, int n) {
+
+void Stitcher::findWarpSizeAndMin(cv::InputArray img1, cv::InputArray img2, cv::Mat &H,
+                                  cv::Mat &W, cv::Size &S, cv::Point &MIN) {
     double h1 = img1.rows(); double w1 = img1.cols();
     double h2 = img2.rows(); double w2 = img2.cols();
-
+    
     // Warp the extrema of img2 to find out where its points would be in
     // a merged image (in img1's coordinates).
     const int dims[] = {4,1};
     double pts1d[] = {0,0,0,h1,w1,h1,w1,0},
-           pts2d[] = {0,0,0,h2,w2,h2,w2,0},
-           pts2_in_perspective_data[8];
+    pts2d[] = {0,0,0,h2,w2,h2,w2,0},
+    pts2_in_perspective_data[8];
     cv::Mat pts1(len(dims), dims, CV_64FC2, pts1d),
-            pts2(len(dims), dims, CV_64FC2, pts2d),
-            pts2_in_perspective(len(dims), dims, CV_64F, pts2_in_perspective_data);
+    pts2(len(dims), dims, CV_64FC2, pts2d),
+    pts2_in_perspective(len(dims), dims, CV_64F, pts2_in_perspective_data);
     cv::perspectiveTransform(pts2, pts2_in_perspective, H);
     
     // Find the extrema of img1 and and the transformed-extrema of img2.
     double max_x = w1, max_y = h1,
-           min_x = 0,  min_y = 0;
+    min_x = 0,  min_y = 0;
     for(int i = 0; i < 4; i++) {
         double py = pts2_in_perspective.at<double>(i,0); // row / y (does Mat.at(..) use 0 == row index and 1 == col??)
         double px = pts2_in_perspective.at<double>(i,1); // col / x
@@ -48,21 +50,26 @@ void Stitcher::warpTwoImages(cv::InputArray img1, cv::InputArray img2, cv::Mat &
     // adding it to max_*:
     min_y = round(min_y - .5); min_x = round(min_x - .5);
     max_y = round(max_y + .5); max_x = round(max_x + .5);
-
+    
     // Build a translation matrix in homogenous coordinates.
     const int Tdims[] = {3,3};
     double Tdata[] =  {1, 0, -min_x, 0, 1, -min_y, 0, 0, 1};
     cv::Mat T(2, Tdims, CV_64F, Tdata);
+    W = T*H;  // Combine the homography and translation into a "Warp".
+    S = cv::Size(max_x - min_x, max_y - min_y);
+    MIN = cv::Point2d(min_x, min_y);
+}
 
-    // Translate and warp img2 so that its pixels align with img1.
+void Stitcher::mergeImageIntoPano(cv::InputArray img, cv::InputArray pano,
+                                  cv::Mat &W, cv::Size &S, cv::Point &MIN,
+                                  cv::OutputArray out) {
+    // Translate and warp pano so that its pixels align with img1.
     cv::Mat warped;
-    cv::warpPerspective(img2, warped, T*H, cv::Size(max_x - min_x, max_y - min_y));
-    char fname[] = {'d','a','t','a','/','w','\0','.','j','p','g','\0'};
-    fname[6] = '0' + n;
-    cv::imwrite(fname, warped);
-
+    cv::warpPerspective(pano, warped, W, S);
+    
     // Copy the pixels of img1 over warped-img2
-    img1.copyTo(warped(cv::Rect(-min_x, -min_y, w1, h1)));
+    double wi = img.cols(), hi = img.rows();
+    img.copyTo(warped(cv::Rect(-MIN.x, -MIN.y, wi, hi)));
     warped.copyTo(out);
 }
 
@@ -73,16 +80,16 @@ void Stitcher::findHomographyMatrix(cv::InputArray img1, cv::InputArray img2, cv
     cv::Mat des1, des2;
     detector->detectAndCompute(img1, /*mask:*/ cv::noArray(), kp1, des1);
     detector->detectAndCompute(img2, /*mask:*/ cv::noArray(), kp2, des2);
-
+    
     // Find correspondence between the descriptors using k-nearest-neighbors.
     cv::BFMatcher bf;
     DescriptorMatches matches;
     bf.knnMatch(des1, des2, matches, 2);
-
+    
     // Filter the matches and extract their image coordinates.
     ImgCoords p1, p2;
     filterMatchesAndExtractPoints(kp1, kp2, matches, .4*img1.rows(), p1, p2);
-
+    
     H = cv::findHomography(p1, p2,
                            cv::RANSAC, /*RANSACreprojThresh:*/5.0
                            /*mask:cv::noArray()*/
@@ -91,20 +98,15 @@ void Stitcher::findHomographyMatrix(cv::InputArray img1, cv::InputArray img2, cv
 
 void Stitcher::stitch(Mats &images, cv::Mat &pano) {
     images[0].copyTo(pano);
-    cv::imwrite("data/p1.jpg", pano);
     int i = 0;
     for(auto img : images) {
         if (++i == 1) continue;
-        std::cout << "\n\nStiching image " << i << " into pano:";
-        cv::Mat H;
+        cv::Mat H, W;
+        cv::Size S;
+        cv::Point MIN;
         findHomographyMatrix(pano, img, H);
         if (H.empty()) continue;    // Failed to find homography; omit the image.
-        std::cout << "\n\tFound homography.\n\tMerging new image to pano.";
-        warpTwoImages(img, pano, H, pano, i);
-        std::cout << "\n\tImage merged.";
-        char fname[] = {'d','a','t','a','/','p','\0','.','j','p','g','\0'};
-        fname[6] = '0' + i;
-        cv::imwrite(fname, pano);
+        findWarpSizeAndMin(img, pano, H, W, S, MIN);
+        mergeImageIntoPano(img, pano, W, S, MIN, pano);
     }
-    std::cout << "\n\nPano complete.\n\n";
 }
--- prototypes/cylindricalimage/cylindricalimage/Stitcher.hpp
+++ prototypes/cylindricalimage/cylindricalimage/Stitcher.hpp
@@ -5,22 +5,23 @@
 #include "opencv2/opencv.hpp"
 /*-
  namespace cv {
-    class Mat; class Point2f; class KeyPoint; class DMatch;
-    class InputArray; class OutputArray;
-}
--*/
+ class Mat; class Point2f; class KeyPoint; class DMatch;
+ class InputArray; class OutputArray;
+ }
+ -*/
 namespace Stitcher {
     typedef std::vector<std::vector<cv::DMatch> > DescriptorMatches;
     typedef std::vector<cv::KeyPoint> KeyPoints;
-
+    
     typedef cv::Point2f ImgCoord;
     typedef std::vector<cv::Point2f> ImgCoords;
     typedef std::vector<cv::Mat> Mats;
-
+    
     void stitch(Mats &images, cv::Mat &pano);
-
+    
     void filterMatchesAndExtractPoints(KeyPoints &kp1, KeyPoints &kp2, DescriptorMatches &matches, double threshold, ImgCoords &p1, ImgCoords &p2);
-    void warpTwoImages(cv::InputArray &img1, cv::InputArray &img2, cv::Mat &H, cv::OutputArray out, int n);
+    void findWarpSizeAndMin(cv::InputArray img1, cv::InputArray img2, cv::Mat &H, cv::Mat &W, cv::Size &S, cv::Point &MIN);
+    void mergeImageIntoPano(cv::InputArray img, cv::InputArray pano, cv::Mat &W, cv::Size &S, cv::Point &MIN, cv::OutputArray out);
     void findHomographyMatrix(cv::InputArray img1, cv::InputArray img2, cv::Mat &H);
 };
 
